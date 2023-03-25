e54b1a34bafe43be16d86e7d97497b18c402d3cb
vladman25 <vladandreiman@gmail.com>
2023-03-25 16:11:14 +0200

    tesT

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -1,9 +1,12 @@
 import "./Landing.scss"
+import {NavLink} from "react-router-dom";
 
 function Landing() {
     return (
         <div>
             <h1>Landing</h1>
+            <NavLink to={"/login"}/>
+            <NavLink to={"/register"}/>
         </div>
     );
 }
