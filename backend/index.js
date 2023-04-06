547484210eb57715948a94593ad709e02ab1c652
vladman25 <vladandreiman@gmail.com>
2023-04-06 19:15:05 +0300

    fix4

--- backend/index.js
+++ backend/index.js
@@ -18,18 +18,22 @@ app.use(express.json());
 const mainRoutes = require("./routes/api.routes")
 app.use("/api", mainRoutes)
 
+mongoose.connect(process.env.MONGO_URL, () => {
+    console.log("Mongoose initialized")
+})
 
 if (process.env.NODE_ENV === 'development') {
     http.createServer(app);
+    app.listen(process.env.PORT, () => {
+        console.log("Server running on port:" + process.env.PORT)
+    })
 } else if (process.env.NODE_ENV === 'production') {
     https.createServer({
         key: fs.readFileSync(process.env.SSL_PRIVATE_KEY_PATH),
         cert: fs.readFileSync(process.env.SSL_CERT_PATH)
-    }, app)
+    }, app).listen(process.env.PORT, function () {
+        console.log(`[server.js] Server running fine on ${process.env.PORT}!`);
+    })
 }
 
 
-app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
-mongoose.connect(process.env.MONGO_URL, () => {
-    console.log("Mongoose initialized")
-})
